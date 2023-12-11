c143f9396101baca5c34662ac6116e0c49302294
m4dox21 <132065931+m4dox21@users.noreply.github.com>
2023-12-11 14:52:50 +0100

    Update myvector.cpp

--- myvector.cpp
+++ myvector.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 
@@ -9,7 +9,7 @@ class myVector {
     int current_size;
 
 public:
-    myVector() : max_size(10), current_size(0) 
+    myVector() : max_size(10), current_size(0)
     {
         array = new T[max_size];
     }
@@ -17,15 +17,7 @@ public:
         delete[] array;
     }
 
-    T* begin() const {
-        return array;
-    }
-
-    T* end() const {
-        return array + current_size;
-    }
-
-    void push_back(T data) {
+    void push_back(T value) {
         if(current_size == max_size)
         {
             T* temp = new T[2 * max_size];
@@ -39,10 +31,9 @@ public:
             array = temp;
         }
 
-        array[current_size] = data;
+        array[current_size] = value;
         current_size++;
     }
-
 
     int size()
     {
@@ -54,20 +45,21 @@ public:
         return max_size;
     }
 
-    void print()
-    {
-        for(int i=0;i<current_size;i++)
-        {
-            cout<<array[i]<<" ";
-        }
+    T* begin(){
+        return array;    
     }
-
-    void reverse_print()
+    
+    T* end(){
+        return array + current_size;
+    }
+    
+    T& operator[](T index)
     {
-        for(int i=current_size-1;i>=0;i--)
+        if(index >= current_size)
         {
-            cout<<array[i]<<" ";
+            throw("Index poza tablica");
         }
+        return array[index];
     }
 };
 
@@ -77,24 +69,23 @@ int main()
     ios_base::sync_with_stdio(false);
     
     myVector<unsigned int> v;
-    int n;
-    
+    unsigned int n;
+
     do
     {
         cin>>n;
-        if(n!=0)
+        if(n>0)
             v.push_back(n);
     } while(n!=0);
-
-    // v.print();
-    // cout<<"\n";
-    // v.reverse_print();
-    // cout<<"\n"<<v.size()<<" / "<<v.capacity();
-
-    for(const auto& value: v)
+    
+    for(auto i = v.end()-1; i > v.begin()-1; i--)
     {
-        cout<<value<<" ";
+        cout<<*i<<" ";
     }
-
+    
+    // cout<<"\n";
+    // cout<<v[1];   
+    // cout<<"\n"<<v.size()<<" / "<<v.capacity();
+    
     return 0;
 }
